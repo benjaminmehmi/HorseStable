91b46d80b79f012af030dcbe2f11474565a0f268
Benjamin Mehmi <benjaminmehmi@hotmail.com>
2025-12-19 14:12:09 +0100

    init

---
+++ HorseStable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HorseStable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.ShowMenu();
+        }
+    }
+}
---
+++ HorseStable/Stable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HorseStable
+{
+    public class Stable
+    {
+        private List<Horse> Horses { get; set; }
+        public Stable()
+        {
+            Horses = new List<Horse>();
+        }
+
+        public void AddHorse(Horse horse)
+        {
+            Horses.Add(horse);
+        }
+    }
+}
